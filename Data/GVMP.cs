2a596ae9fd49a1db9c6af1f214e649b7a5232cf3
Mario Franze <mfranze@germandev.de>
2018-12-04 20:03:55 +0100

    + Added better error handling

--- Data/GVMP.cs
+++ Data/GVMP.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Security.Cryptography;
+using Microsoft.Win32;
 using Newtonsoft.Json.Linq;
 
 namespace MyVMP_Launcher.Data
@@ -10,6 +11,7 @@ namespace MyVMP_Launcher.Data
     {
         const string PatchURL = "http://server1.gvmp.de:5000/api/patching";
         const string LiveURL = "http://launcher.gvmp.de/live/";
+		public static string UserName { get; private set; } = string.Empty;
         static WebClient Client = new WebClient();
 
 		/// <summary>
@@ -18,9 +20,32 @@ namespace MyVMP_Launcher.Data
 		/// <param name="FileName">Name of the DLC</param>
         static void Download(string FileName)
         {
-            Helper.Logging.Log(string.Format("Downloading DLC: {0}", FileName));
-            Client.DownloadFile(string.Format("{0}{1}/dlc.rpf", LiveURL, FileName), string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
-            Helper.Logging.Log(string.Format("Downloading of DLC: {0} finished", FileName));
+			try
+			{
+				Helper.Logging.Log(string.Format("Downloading DLC: {0}", FileName));
+				Client.DownloadFile(string.Format("{0}{1}/dlc.rpf", LiveURL, FileName), string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
+				Helper.Logging.Log(string.Format("Downloading of DLC: {0} finished", FileName));
+			}
+			catch (Exception ex)
+			{
+				if (Convert.ToInt32(FileName) == 0)
+				{
+					Helper.Logging.Log(string.Format("Fehler beim Download der Datei: {0}{1}/dlc.rpf", LiveURL, FileName), "error");
+					Helper.Logging.Log(string.Format("Fehler: {0}", ex.Message), "error");
+				}
+				else
+				{
+					Helper.Logging.Log(string.Format("Could not download DLC {0}. It looks like the Updater and can be ignored... maybe...", FileName), "warning");
+					try
+					{
+						Directory.Delete(string.Format("{0}{1}", RAGE.DLCPacks, FileName));
+					}
+					catch (Exception e)
+					{
+						Helper.Logging.Log(string.Format("Fehler: {0}", e.Message), "error");
+					}
+				}
+			}
         }
 
 		/// <summary>
@@ -31,12 +56,24 @@ namespace MyVMP_Launcher.Data
 		/// <returns></returns>
 		static bool IsLatest(string FileName, string Hash)
 		{
-			MD5 md5 = MD5.Create();
-			FileStream stream = File.OpenRead(string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
-			stream.Position = 0L;
-			string fileHash = BitConverter.ToString(md5.ComputeHash((Stream)stream)).Replace("-", "").ToLowerInvariant();
-			stream.Close();
-			return fileHash == Hash;
+			try
+			{
+				if (File.Exists(string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName)))
+				{
+					MD5 md5 = MD5.Create();
+					FileStream stream = File.OpenRead(string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
+					stream.Position = 0L;
+					string fileHash = BitConverter.ToString(md5.ComputeHash((Stream)stream)).Replace("-", "").ToLowerInvariant();
+					stream.Close();
+					return fileHash == Hash;
+				}
+				return false;
+			}
+			catch (Exception ex)
+			{
+				Helper.Logging.Log(string.Format("Fehler beim Abgleich: {0}", ex.Message), "error");
+				return false;
+			}
 		}
 
 		/// <summary>
@@ -44,7 +81,17 @@ namespace MyVMP_Launcher.Data
 		/// </summary>
         public static void Init()
         {
-            Helper.Logging.Log("Checking DLC's");
+			Helper.Logging.Log("Getting PlayerName");
+			try
+			{
+				RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\RAGE-MP");
+				UserName = (string)registryKey.GetValue("player_name");
+			}
+			catch (Exception ex)
+			{
+				Helper.Logging.Log(ex.Message);
+			}
+			Helper.Logging.Log("Checking DLC's");
             string json = Client.DownloadString(PatchURL);
             JObject results = JObject.Parse(json);
             foreach (var item in results)
