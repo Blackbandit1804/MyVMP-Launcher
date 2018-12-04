c980accd56a053ca29c625a50e1146a86578458d
Mario Franze <mfranze@germandev.de>
2018-12-04 20:04:06 +0100

    * Initial commit

---
+++ Data/Links.cs
@@ -0,0 +1,14 @@
+﻿namespace MyVMP_Launcher.Data
+{
+	class Links
+	{
+		public const string Board = "https://www.gvmp.de/forum/";
+		public const string Discord = "https://discord.gg/TBDXyXK";
+		public const string Support = "https://www.gvmp.de/ticketsystem/";
+		public const string TSIdent = "https://www.gvmp.de/index.php?team-speak-id-list/";
+		public const string TSAddress = "ts3server://ts.gvmp.de/";
+		public const string UCP = "https://www.gvmp.de/index.php?ucp-character/";
+		public const string WhiteList = "https://www.gvmp.de/index.php?whitelist/";
+		public const string Wiki = "https://www.gvmp.de/wiki/";
+	}
+}
