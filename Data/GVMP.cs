8543ef83a00de01903bc9156de97561477b67807
Mario Franze <mfranze@germandev.de>
2018-12-10 20:03:14 +0100

    * Changed download function

--- Data/GVMP.cs
+++ Data/GVMP.cs
@@ -18,35 +18,40 @@ namespace MyVMP_Launcher.Data
 		/// Downloads the given DLC
 		/// </summary>
 		/// <param name="FileName">Name of the DLC</param>
-        static void Download(string FileName)
+        static void Download(string FileName, string type)
         {
-			try
+			switch(type)
 			{
-				Helper.Logging.Log(string.Format("Downloading DLC: {0}", FileName));
-				Client.DownloadFile(string.Format("{0}{1}/dlc.rpf", LiveURL, FileName), string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
-				Helper.Logging.Log(string.Format("Downloading of DLC: {0} finished", FileName));
-			}
-			catch (Exception ex)
-			{
-				if (Convert.ToInt32(FileName) == 0)
-				{
-					Helper.Logging.Log(string.Format("Fehler beim Download der Datei: {0}{1}/dlc.rpf", LiveURL, FileName), "error");
-					Helper.Logging.Log(string.Format("Fehler: {0}", ex.Message), "error");
-				}
-				else
-				{
-					Helper.Logging.Log(string.Format("Could not download DLC {0}. It looks like the Updater and can be ignored... maybe...", FileName), "warning");
+				case "dlc":
 					try
 					{
-						Directory.Delete(string.Format("{0}{1}", RAGE.DLCPacks, FileName));
+						Helper.Logging.Log(string.Format("Downloading DLC: {0}", FileName));
+						Client.DownloadFile(string.Format("{0}{1}/dlc.rpf", LiveURL, FileName), string.Format("{0}{1}/dlc.rpf", RAGE.DLCPacks, FileName));
+						Helper.Logging.Log(string.Format("Downloading of DLC: {0} finished", FileName));
 					}
-					catch (Exception e)
+					catch (Exception ex)
 					{
-						Helper.Logging.Log(string.Format("Fehler: {0}", e.Message), "error");
+						if (Convert.ToInt32(FileName) == 0)
+						{
+							Helper.Logging.Log(string.Format("Fehler beim Download der Datei: {0}{1}/dlc.rpf", LiveURL, FileName), "error");
+							Helper.Logging.Log(string.Format("Fehler: {0}", ex.Message), "error");
+						}
+						else
+						{
+							Helper.Logging.Log(string.Format("Could not download DLC {0}. It looks like the Updater and can be ignored... maybe...", FileName), "warning");
+							try
+							{
+								Directory.Delete(string.Format("{0}{1}", RAGE.DLCPacks, FileName));
+							}
+							catch (Exception e)
+							{
+								Helper.Logging.Log(string.Format("Fehler: {0}", e.Message), "error");
+							}
+						}
 					}
-				}
+					break;
 			}
-        }
+		}
 
 		/// <summary>
 		/// Checks the MD5 hast of a file
@@ -96,20 +101,23 @@ namespace MyVMP_Launcher.Data
             JObject results = JObject.Parse(json);
             foreach (var item in results)
             {
-                string currentFolder = RAGE.DLCPacks + item.Key;
-                Helper.Logging.Log(string.Format("Checking: {0}", item.Key));
-                if (!Directory.Exists(currentFolder))
-                {
-                    Helper.Logging.Log(string.Format("Creating: {0}", item.Key));
-                    Directory.CreateDirectory(currentFolder);
-                    Download(item.Key);
-                }
-				else
+				if (!int.TryParse(item.Key, out int n))
 				{
-					if(!IsLatest(item.Key, item.Value.ToString()))
+					string currentFolder = RAGE.DLCPacks + item.Key;
+					Helper.Logging.Log(string.Format("Checking: {0}", item.Key));
+					if (!Directory.Exists(currentFolder))
 					{
-						Helper.Logging.Log(string.Format("Updating: {0}", item.Key));
-						Download(item.Key);
+						Helper.Logging.Log(string.Format("Creating: {0}", item.Key));
+						Directory.CreateDirectory(currentFolder);
+						Download(item.Key, "dlc");
+					}
+					else
+					{
+						if (!IsLatest(item.Key, item.Value.ToString()))
+						{
+							Helper.Logging.Log(string.Format("Updating: {0}", item.Key));
+							Download(item.Key, "dlc");
+						}
 					}
 				}
             }
