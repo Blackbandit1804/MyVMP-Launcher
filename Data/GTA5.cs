36da1f49d59fdb007834324b279622182f1fd766
Mario Franze <mfranze@germandev.de>
2018-12-13 16:58:16 +0100

    + Added SetNewBasePath() * Bug fixes

--- Data/GTA5.cs
+++ Data/GTA5.cs
@@ -20,8 +20,24 @@ namespace MyVMP_Launcher.Data
             }
             catch (System.Exception ex)
             { 
-                Helper.Logging.Log(ex.Message);
+                Helper.Logging.Log(ex.Message, "error");
             }
         }
+
+		public static void SetNewBasePath(string path)
+		{
+			Helper.Logging.Log("Trying to set GTA5 install folder ");
+			try
+			{
+				RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\RAGE-MP", true);
+				registryKey.SetValue("game_v_path", path, RegistryValueKind.String);
+				Base = string.Format("{0}\\", path);
+				Helper.Logging.Log(string.Format("Successfully changed GTA5 install folder to \"{0}\"", Base));
+			}
+			catch (System.Exception ex)
+			{
+				Helper.Logging.Log(ex.Message, "error");
+			}
+		}
     }
 }
