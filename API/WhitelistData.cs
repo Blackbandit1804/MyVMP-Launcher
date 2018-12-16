add81003da4f3a064ab0136ce95bb69954e3be84
Mario Franze <mfranze@germandev.de>
2018-12-16 21:24:36 +0100

    * Initial commit

---
+++ API/WhitelistData.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVMP_Launcher.API
+{
+	public class Whitelist
+	{
+		public string m_Key = "163FBAJF936AHGKS837ABKSJ9913HSAAG";
+		public string m_AccountData;
+		public string m_Hash = "0";
+	}
+}
